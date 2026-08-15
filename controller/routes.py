36bfecc5240c054cddac042fcce3f7764c3358c2
iurycar <iurycardoso2001@gmail.com>
2026-08-15 13:11:51 -0300

    refactor: remoção dos arquivos redundantes 'responsabilidade'

--- controller/routes.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint, jsonify, request, session
-
-# Criação do Blueprint para as rotas
-routes_bp = Blueprint('routes', __name__)
--- models/responsabilidade.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Responsabilidade:
-    id: int
-    id_usuario: int
-    id_setor: int
