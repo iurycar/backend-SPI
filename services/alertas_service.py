de0f2df340d63605314848dc4fe6babf44338f23
iurycar <iurycardoso2001@gmail.com>
2026-09-04 15:12:57 -0300

    feat: implementação da notificação por e-mail

--- services/alertas_service.py
+++ services/alertas_service.py
@@ -1,9 +1,15 @@
 from repository.alertas_repository import AlertasRepository
+from services.usuario_service import UsuarioService
+
+from email.message import EmailMessage
+import smtplib
+import os
 
 class AlertasService:
     def __init__(self, connection):
         self.connection = connection
         self.alertas_repository = AlertasRepository(connection)
+        self.usuario_service = UsuarioService(connection)
 
     def obter_alertas(self) -> list[dict] | None:
         alertas = self.alertas_repository.get_alertas()
@@ -98,6 +104,31 @@ class AlertasService:
         return self.alertas_repository.marcar_alerta_resolvido(id_alerta)
 
     def criar_alerta(self, id_monitorar: int, id_usuario: int | None, evento: str, severidade: int = 1) -> bool:
+        if severidade == 3:
+            email = self.usuario_service.get_email_usuario_por_id(id_usuario)
+            if email:
+
+                email_address = os.getenv('EMAIL_ADDRESS')
+                token_senha = os.getenv('EMAIL_PASSWORD')
+
+                if email_address and token_senha:
+                    msg = EmailMessage()
+
+                    corpo_email = f"Alerta de severidade crítico, na zona de monitoramento {id_monitorar}!\n\nEvento: {evento}\nSeveridade: {severidade}\n\nPor favor, tome as medidas necessárias."
+
+                    msg["Subject"] = "Alerta Crítico"
+                    msg["From"] = email_address
+                    msg["To"] = email
+
+                    msg.set_content(corpo_email)
+
+                    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+                        server.starttls()
+                        server.login(email_address, token_senha)
+                        server.send_message(msg)
+
+            # TODO: Adicionar a lógica para enviar os alertas para WebSocket
+
         return self.alertas_repository.criar_alerta(id_monitorar, id_usuario, evento, severidade)
 
     def deletar_alerta(self, id_alerta: int) -> bool:
